712b1ded4ef511a5f48eb8bf031cf94dc8826940
Christian Taedcke <hacking@taedcke.com>
2019-03-12 11:01:27 +0100

    Add shell script

---
+++ create_svd.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+DOCS_FOLDER="./docs"
+RM_FILENAME="$DOCS_FOLDER/SiM3U1xx-SiM3C1xx-RM.pdf"
+RM_LINK=https://www.silabs.com/documents/public/data-sheets/SiM3U1xx-SiM3C1xx-RM.pdf
+SVD_TMP_OUT="$(mktemp)"
+SVD_OUT=./sim3u.svd
+
+mkdir -p $DOCS_FOLDER
+
+if [ ! -f $RM_FILENAME ]; then
+    wget $RM_LINK -O $RM_FILENAME
+fi
+
+pipenv install
+pipenv run python src/parse_sim3u.py --input $RM_FILENAME --out $SVD_TMP_OUT
+xmllint --format $SVD_TMP_OUT > $SVD_OUT
+rm $SVD_TMP_OUT
